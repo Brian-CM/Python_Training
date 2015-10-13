13ab54f68331f647b2ab3b1d3a47727c364e7630
Brian <brian.cox@catalinamarketing.com>
2015-10-13 17:01:06 -0400

    Create Week_1_Python.py
    The assignment for week 1. I am still working on the random array.

---
+++ Week_1_Python.py
@@ -0,0 +1,19 @@
+hundred = range(100)
+
+Descending Array
+print sorted(hundred, reverse=True)
+
+Ascending Array
+print list(hundred)
+
+#ForLoops
+for n in reversed(range(100)):
+    print n
+
+#Random Array
+#Still working on this one!!!
+
+#Print Name reveresed
+name = ['Brian', 'Cox']
+name.reverse()
+print " :", name
