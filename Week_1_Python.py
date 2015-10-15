065fa85b4fa1f991611642f844b6f5f360105f42
Brian <brian.cox@catalinamarketing.com>
2015-10-15 15:54:18 -0400

    Update Week_1_Python.py

--- Week_1_Python.py
+++ Week_1_Python.py
@@ -11,7 +11,13 @@ for n in reversed(range(100)):
     print n
 
 #Random Array
-#Still working on this one!!!
+import random
+fifty = random.sample(range(0, 50), 50)
+max_val = max(fifty)
+min_val = min(fifty)
+print fifty
+print max_val
+print min_val
 
 #Print Name reveresed
 name = ['Brian', 'Cox']
